85a3b56238ebbcd1a0d08321f2f0c54fc0afa99a
Stanislav Bukhgolts <sbuchholz@yandex.ru>
2021-04-03 18:38:12 +0300

    ff

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
-  const [selectedCard, setSelectedCard] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null??false);
   const [cards, setCards] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
 
